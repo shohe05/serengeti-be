476b40848266b7c9fa88b488ca36079c418a3289
田中将平 <syouhei@jobby-MBP.local>
2018-06-15 21:05:58 +0900

    Initial commit. Initialize Spring Boot project.

---
+++ src/main/java/com/shotana/serengetibe/SerengetiBeApplication.java
@@ -0,0 +1,12 @@
+package com.shotana.serengetibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerengetiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerengetiBeApplication.class, args);
+    }
+}
